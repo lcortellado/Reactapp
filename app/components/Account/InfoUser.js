f0dab493b864daf440600fcfde095c71b2f99241
Luis Carlos Ortellado Cabral <luiscarlosortecabral@gmail.com>
2020-01-09 14:37:32 -0300

    avatar usuario

--- app/components/Account/InfoUser.js
+++ app/components/Account/InfoUser.js
@@ -1,12 +1,70 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { Avatar } from 'react-native-elements';
-import { func } from 'prop-types';
+import * as firebase from "firebase";
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
 
-export default function InfoUser() {
+
+export default function InfoUser(props) {
+    const {
+         userInfo: { uid, displayName, email, photoURL } 
+        } = props;
+
+        const changeAvatar = async () => {
+           const resultPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+           const resultPermissionCamera = resultPermission.permissions.CAMERA_ROLL.status;
+           
+           if (resultPermissionCamera === "denied") {
+            console.log("ES NECESARIO ACEPTAR");
+           } else {
+               const result = await ImagePicker.launchImageLibraryAsync({
+                   allowsEditing: true,
+                   aspect: [4, 3]
+               });
+           }
+           
+        };
+
     return (
-        <View>
-            <Text>InfoUser...</Text>
+        <View style={styles.ViewUserInfo}>
+            <Avatar 
+                rounded
+                size="large"
+                showEditButton
+                onEditPress={changeAvatar}
+                containerStyle={styles.userInfoAvatar}
+                source={{
+                    uri: photoURL ? photoURL : "https://api.adorable.io/avatars/266/abott@adorable.png"
+                }}
+            />
+            <View style={styles.viewUserInfo}>
+
+                <Text style={styles.displayName}>
+
+                    {displayName ? displayName : "Anonimo"}
+                </Text>
+                 
+                <Text>{email}</Text>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    viewUserInfo: {
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "row",
+        backgroundColor: "#f2f2f2",
+        paddingTop: 30,
+        paddingBottom: 30
+
+    },
+    userInfoAvatar: {
+        marginRight: 20
+    }, 
+    displayName: {
+        fontWeight: 'bold'
+    }
+});
--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -5,17 +5,18 @@ import * as firebase from 'firebase';
 import InfoUser from '../../components/Account/InfoUser';
 
 export default function UserLogged() {
-    const [userInfo, setUserInfo] = useState({});(
+    const [userInfo, setUserInfo] = useState({});
 
     useEffect(() => {
         (async () => {
             const user = await firebase.auth().currentUser;
             setUserInfo(user.providerData[0]);
-        })
-    })
+        })();
+    }, []);
+
     return(
         <View>
-            <InfoUser />
+            <InfoUser userInfo={userInfo} />
             <Button title="Cerrar sesion" onPress={() => firebase.auth().signOut()} />
         </View>
     );
