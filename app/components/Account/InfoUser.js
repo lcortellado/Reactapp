6b3b2ea60a32bdfe024ee6d01a67f406a13a7ced
Luis Carlos Ortellado Cabral <luiscarlosortecabral@gmail.com>
2020-01-08 23:29:20 -0300

    push

---
+++ app/components/Account/InfoUser.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { Avatar } from 'react-native-elements';
+import { func } from 'prop-types';
+
+export default function InfoUser() {
+    return (
+        <View>
+            <Text>InfoUser...</Text>
+        </View>
+    );
+}
--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from "react-native";
 import { Button } from "react-native-elements";
 import * as firebase from 'firebase';
+import InfoUser from '../../components/Account/InfoUser';
 
 export default function UserLogged() {
+    const [userInfo, setUserInfo] = useState({});(
+
+    useEffect(() => {
+        (async () => {
+            const user = await firebase.auth().currentUser;
+            setUserInfo(user.providerData[0]);
+        })
+    })
     return(
         <View>
-            <Text>UserLogged...</Text>
+            <InfoUser />
             <Button title="Cerrar sesion" onPress={() => firebase.auth().signOut()} />
         </View>
     );
